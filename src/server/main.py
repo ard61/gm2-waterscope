c7762837b12969652b540931ce74ef20aa2c9e39
Antoine Dupuis <ard61@cam.ac.uk>
2017-06-01 13:07:59 +0100

    Split off the webpage in a template, templates/index.html, and added function to start streamer with new resolution

--- src/server/main.py
+++ src/server/main.py
@@ -1,35 +1,46 @@
 import subprocess
+import flask
 
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return """<!DOCTYPE html>
-<html>
-
-<head>
-  <title>Flask + MJPEG streaming test</title>
-</head>
-
-<body>
-  <img width="640" height="480" src="http://172.24.1.1:8080/?action=stream" />
-</body>
-
-</html>
-"""
+app = flask.Flask(__name__)
 
 class MJpegStream():
     def __init__(self):
-        self.mjpg_streamer_args = ['cd ~/mjpg-streamer-master/mjpg-streamer-experimental && ./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -fps 10 -x 1280 -y 720"']
         self.proc = None
 
-    def start(self):
-        #self.proc = subprocess.Popen(self.vlc_args, stdin=None, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        self.proc = subprocess.Popen(self.mjpg_streamer_args, shell=True)
+    def start(self, *args, **kwargs):
+        if self.proc:
+            self.stop()
+        self.proc = subprocess.Popen(self.create_args(*args, **kwargs), cwd='~/mjpg-streamer-master/mjpg-streamer-experimental')
 
     def stop(self):
         self.proc.terminate()
 
+    def create_args(self, width=1280, height=720, fps=10):
+        return ['mjpg_streamer', '-o', 'output_http.so -w ./www', '-i', 'input_raspicam.so -x {} -y {} -fps {}'.format(width, height, fps)]
+
 stream = MJpegStream()
-stream.start()
+
+@app.route('/', methods=['GET'])
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/start_stream', methods=['GET'])
+def start_stream():
+    get_args = flask.request.args
+    width = get_args.get('width')
+    height = get_args.get('height')
+    fps = get_args.get('fps')
+
+    # Do some error checking
+    supported_resolutions = [(1280, 720),
+                              (1920, 1080),
+                              (640, 480),
+                              (320, 240)]
+    if (width, height) not in supported_resolutions:
+        width, height = 1280, 720
+
+    supported_fps = range(1, 30)
+    if fps not in supported_fps:
+        fps = 10
+
+    stream.start(width=width, height=height, fps=fps)
