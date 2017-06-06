31ee607a80c2f054e94f866014ae58d7064fa92d
Antoine Dupuis <ard61@cam.ac.uk>
2017-06-06 22:46:16 +0000

    Added app.run() to run the server when executed as python3 main.py

--- src/server/main.py
+++ src/server/main.py
@@ -214,3 +214,6 @@ def microswitch():
         return flask.jsonify({"microswitch": "on"})
     else:
         return flask.jsonify({"microswitch": "off"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, threaded=True)
